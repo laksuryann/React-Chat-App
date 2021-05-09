12b1cd20b59e2915d0e68e6ce80bd7a5bcfceae9
venkatesh dharmavarapu <venkatadharma1@gmail.com>
2021-05-09 20:57:17 +0530

    App.js is updated with ChatEngine's renderChatFeed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from "react-chat-engine"
 
+import ChatFeed from "./components/ChatFeed"
+
 import "./App.css"
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
       projectID="317adac9-5e8c-4402-bb27-7d23f163dd02"
       userName="Venkatesh"
       userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps } />}
     />
   );
 }
